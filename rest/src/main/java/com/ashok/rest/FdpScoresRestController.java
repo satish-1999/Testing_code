58584424f82969633621bd66c5abcfe9acb5be21
satish-1999 <schandra1.sc@gmail.com>
2024-04-30 21:47:43 +0530

    Adding rest Folder

---
+++ rest/src/main/java/com/ashok/rest/FdpScoresRestController.java
@@ -0,0 +1,16 @@
+package com.ashok.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fpd")
+public class FdpScoresRestController 
+{
+	@GetMapping("/score")
+	public String getFpdScores(@RequestParam(value="ssn" , required=false, defaultValue ="23479")Long ssn) {
+		return "FPD is "+ssn;
+	}
+}
